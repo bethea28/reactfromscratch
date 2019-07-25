5b25a14f6e83ef69aa927212eab0dc7c6f83bfcb
Bryan Bethea <bryanbethea28@gmail.com>
2019-07-25 12:27:35 -0400

    test

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
+class Test extends React.Component {
+    render() {
+        return <div>test boy</div>;
+    }
+}
 
 const App = () => {
-    return <div>Hello PUNKS!</div>;
+    return (
+        <div>
+            Hello PUNKS!
+            <Test />
+        </div>
+    );
 };
-
-// class App extends React.Component {
-//     render = () => {
-//         return (<div>test boy</div>;)
-//     };
-// }
 
 render(<App />, document.getElementById("index"));
